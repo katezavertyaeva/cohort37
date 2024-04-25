4977b24223e06f051ba550c620bd19d3cf03993d
Ekaterina Zavertyaeva <katsiarynazavertyaeva@gmail.com>
2024-04-25 19:49:24 +0200

    add consultation 3

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -1,41 +1,60 @@
-import { useState } from "react";
+import { useState } from "react"
 
-import Button from "../Button/Button";
-import "./styles.css";
-import { products } from "./data";
+import Button from "../Button/Button"
+import "./styles.css"
+import { products } from "./data"
 
 function ProductsList() {
-  const [chosenProducts, setChosenProducts] = useState([]);
+    const [chosenProducts, setChosenProducts] = useState([])
 
-  const productsArray = products.map((product) => {
-    return (
-      <div key={product.id} className="list-entry">
-        <p>{product.name}</p>
-        <Button name="Add" onButtonClick={() => addProduct(product.id)} />
-        <Button name="Remove" onButtonClick={() => {}} />
-      </div>
-    );
-  });
-
-  const addProduct = (id) => {
-    let searchedEl = products.find((product) => {
-      return product.id === id;
-    });
-    setChosenProducts(searchedEl);
-
-    console.log(searchedEl);
+    const productsArray = products.map((product) => {
+        return (
+            <div key={product.id} className="list-entry">
+                <p >{product.name}</p>
+                <Button name="Add" onButtonClick={() => addProduct(product.id)} />
+                <Button name="Remove" onButtonClick={() => removeProduct(product.id)} />
+            </div>
+        )
+    }
+    )
     console.log(chosenProducts);
-  };
 
-  return (
-    <div className="products-list-wrapper">
-      <h4>Products List:</h4>
-      <div className="products-list">{productsArray}</div>
+    const addProduct = (id) => {
 
-      <h4>Selected Products:</h4>
-      <div className="selected-products"></div>
-    </div>
-  );
+        let searchedEl = products.find((product) => {
+            return product.id === id
+        })
+        setChosenProducts(prevProducts => [...prevProducts, searchedEl])
+
+    }
+
+
+    const removeProduct = (id) => {
+
+        let filteredProducts = chosenProducts.filter((selectedItem) => selectedItem.id !== id);
+   
+        setChosenProducts(filteredProducts)
+        console.log(filteredProducts)
+
 }
 
-export default ProductsList;
+const selectedItems = chosenProducts.map((product) => {
+    return (
+        <p key={product.id} className="list-entry">{product.name}</p>
+    )
+})
+
+return (
+    <div className="products-list-wrapper">
+
+        <h4>Products List:</h4>
+        <div className="products-list">{productsArray}</div>
+
+        <h4>Selected Products:</h4>
+        <div className="selected-products">{selectedItems}</div>
+
+    </div>
+)
+}
+
+export default ProductsList
